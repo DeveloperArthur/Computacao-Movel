fd73853d169932c624c89a0e40b3f467a2becc1b
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-10-11 17:55:06 -0300

    Add files via upload

---
+++ app8 - ArthurBank/java/com/example/arthurbank/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.arthurbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent it = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(it);
+            }
+        }, 3000);
+    }
+}
